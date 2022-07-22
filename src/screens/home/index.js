5abc7e7ef93bb36f60305dea7dbbec218c1f5ed6
tarik <asdasdsa@mail.com>
2022-07-22 08:43:06 +0300

    screenshot

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,8 +1,8 @@
-
-
-import React, { useState } from 'react';
+// screen shootn sadece cizilen resmi alması
+//  
+import React, { useState, useRef, useEffect, captureRef } from 'react';
 import { View, Text, StyleSheet, Dimensions, TouchableOpacity, Button, StatusBar } from 'react-native';
-import DoubleClick from 'react-native-double-tap';
+import { ViewShot, captureScreen } from "react-native-view-shot";
 
 const Home = () => {
     const { width, height } = Dimensions.get('window');
@@ -28,10 +28,30 @@ const Home = () => {
         setColorArray(tmp);
     }
 
-    return (
-        <View style={styles.main}>
-            <StatusBar barStyle={'light-content'} translucent backgroundColor={'#1f252b'} />
 
+    const [imageURI, setImageURI] = useState(
+        '',
+    );
+    const [savedImagePath, setSavedImagePath] = useState('');
+
+    const takeScreenShot = () => {
+        captureScreen({
+            format: 'jpg',
+            quality: 0.8,
+        }).then(
+            (imageURI) => {
+                setSavedImagePath(imageURI);
+                setImageURI(imageURI);
+            },
+            (error) => console.error('Oops, Something Went Wrong', error),
+        );
+    };
+
+    return (
+
+        <View style={styles.main}>
+
+            <StatusBar barStyle={'light-content'} translucent />
             <View style={styles.pixelContainer}>
                 {rowsArray.map((r, ri) => (
                     <View style={styles.row}>
@@ -45,42 +65,53 @@ const Home = () => {
                                     delay={200}>
                                     <View style={{ width: width / 20, aspectRatio: 1 }}>
                                         <View style={{ width: '86%', aspectRatio: 1, backgroundColor: color, borderRadius: 4, }} />
-                                    </View> 
+                                    </View>
                                 </TouchableOpacity>
                             )
                         })}
                     </View>
                 ))}
             </View>
-    
+
             <View style={styles.buttonContainer}>
                 <View style={{ flexDirection: 'row', }}>
-                    <TouchableOpacity title='Turn color to red!' style={{ borderRadius: 50, margin: width / 20, width: width / 12, aspectRatio: 1, color: 'red', borderWidth: 1, backgroundColor: 'red' }} onPress={() => setColor('red')}>
-                    </TouchableOpacity>
-                    <TouchableOpacity title='Turn color to blue!' style={{ borderRadius: 50, margin: width / 20, width: width / 12, aspectRatio: 1, color: 'blue', borderWidth: 1, backgroundColor: 'blue' }} onPress={() => setColor('blue')}>
-                    </TouchableOpacity>
-                    <TouchableOpacity title='Turn color to green!' style={{ borderRadius: 50, margin: width / 20, width: width / 12, aspectRatio: 1, color: 'green', borderWidth: 1, backgroundColor: 'green' }} onPress={() => setColor('green')}>
-                    </TouchableOpacity>
+                    <TouchableOpacity style={{ borderColor: 'white', width: width / 12, aspectRatio: 1, borderWidth: 1, backgroundColor: 'red' }} onPress={() => setColor('red')}></TouchableOpacity>
+                    <TouchableOpacity style={{ borderColor: 'white', width: width / 12, aspectRatio: 1, borderWidth: 1, backgroundColor: 'blue' }} onPress={() => setColor('blue')}></TouchableOpacity>
+                    <TouchableOpacity style={{ borderColor: 'white', width: width / 12, aspectRatio: 1, borderWidth: 1, backgroundColor: 'green' }} onPress={() => setColor('green')}></TouchableOpacity>
+                    <TouchableOpacity style={{ borderColor: 'white', width: width / 12, aspectRatio: 1, borderWidth: 1, backgroundColor: 'grey' }} onPress={() => setColor('grey')}></TouchableOpacity>
+                    <TouchableOpacity style={{ borderColor: 'white', width: width / 12, aspectRatio: 1, borderWidth: 1, backgroundColor: 'purple' }} onPress={() => setColor('purple')}></TouchableOpacity>
+                    <TouchableOpacity style={{ borderColor: 'white', width: width / 12, aspectRatio: 1, borderWidth: 1, backgroundColor: 'pink' }} onPress={() => setColor('pink')}></TouchableOpacity>
+                    <TouchableOpacity style={{ borderColor: 'white', width: width / 12, aspectRatio: 1, borderWidth: 1, backgroundColor: 'green' }} onPress={() => setColor('green')}></TouchableOpacity>
                 </View>
                 <View style={{ flexDirection: 'row' }}>
-                    <TouchableOpacity title='Turn color to white!' style={{ borderRadius: 50, margin: width / 20, width: width / 12, aspectRatio: 1, color: 'white', borderWidth: 1, backgroundColor: 'white' }} onPress={() => setColor('white')}>
-                    </TouchableOpacity>
-                    <TouchableOpacity title='Turn color to black!' style={{ borderRadius: 50, margin: width / 20, width: width / 12, aspectRatio: 1, color: 'black', borderWidth: 1, backgroundColor: 'black' }} onPress={() => setColor('black')}>
-                    </TouchableOpacity>
-                    <TouchableOpacity title='Turn color to brown!' style={{ borderRadius: 50, margin: width / 20, width: width / 12, aspectRatio: 1, color: 'brown', borderWidth: 1, backgroundColor: 'brown' }} onPress={() => setColor('brown')}>
-                    </TouchableOpacity>
-                    <TouchableOpacity title='Turn color to yellow!' style={{ borderRadius: 50, margin: width / 20, width: width / 12, aspectRatio: 1, color: 'yellow', borderWidth: 1, backgroundColor: 'yellow' }} onPress={() => setColor('yellow')}>
-                    </TouchableOpacity>
+                    <TouchableOpacity title='Turn color to turquoise!' style={{ width: width / 12, aspectRatio: 1, borderColor: 'white', borderWidth: 1, backgroundColor: 'turquoise' }} onPress={() => setColor('turquoise')}></TouchableOpacity>
+                    <TouchableOpacity title='Turn color to black!' style={{ width: width / 12, aspectRatio: 1, borderColor: 'white', borderWidth: 1, backgroundColor: 'black' }} onPress={() => setColor('black')}></TouchableOpacity>
+                    <TouchableOpacity title='Turn color to brown!' style={{ width: width / 12, aspectRatio: 1, borderColor: 'white', borderWidth: 1, backgroundColor: 'brown' }} onPress={() => setColor('brown')}></TouchableOpacity>
+                    <TouchableOpacity title='Turn color to yellow!' style={{ width: width / 12, aspectRatio: 1, borderColor: 'white', borderWidth: 1, backgroundColor: 'yellow' }} onPress={() => setColor('yellow')}></TouchableOpacity>
+                    <TouchableOpacity style={{ borderColor: 'white', width: width / 12, aspectRatio: 1, borderWidth: 1, backgroundColor: 'orange' }} onPress={() => setColor('orange')}></TouchableOpacity>
+                    <TouchableOpacity style={{ borderColor: 'white', width: width / 12, aspectRatio: 1, borderWidth: 1, backgroundColor: '#AAB8C2' }} onPress={() => setColor('#AAB8C2')}></TouchableOpacity>
+                    <TouchableOpacity style={{ borderColor: 'white', width: width / 12, aspectRatio: 1, borderWidth: 1, backgroundColor: '#8B4513' }} onPress={() => setColor('#8B4513')}></TouchableOpacity>
                 </View>
+                <TouchableOpacity onPress={() => onPress = takeScreenShot()}>
+                    <Text> To save the image, click!</Text>
+                </TouchableOpacity>
             </View>
+            <Text style={styles.textStyle}>
+                {
+                    savedImagePath ?
+                        `Saved Image Path\n ${savedImagePath}` : ''
+                }
+            </Text>
         </View>
+
+
     );
 }
 
 const styles = StyleSheet.create({
     main: {
         flex: 1,
-        backgroundColor: '#14171A',
+        backgroundColor: '#9db8d4',
         justifyContent: 'center',
         alignItems: 'center',
     },
